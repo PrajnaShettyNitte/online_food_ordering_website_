cd503ac32443acc14e5fe3fb2b8b7716c16a7304
Prajna Shetty <113994915+PrajnaShettyNitte@users.noreply.github.com>
2023-09-09 11:02:52 +0530

    Files Upload

---
+++ aboutus.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> About | Dream Cafe' </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body background="images/aboutus.jpg">
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Dream Cafe'</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="wide">
+        
+        <div class="tagline"><font color="#ff8c1a"><strong> Where Every </font> </strong><strong> <font color="#e6b800">Flavor Tells</strong></font> <font color="#e6e600"><strong>a Story!! </strong></font></div>
+<div class="tagline"><font color="aqua">Dream Cafe' </font> </div>        
+
+        <br>
+        <h2 style="color: white;"><br>Welcome to our online food website....!</h2><br><br>
+<h3 style="color:wheat;">We understand that your time is precious, which is why we have streamlined the ordering process for ultimate convenience. With our user-friendly interface, you can effortlessly navigate through our virtual aisles, customizing your order to perfection. Explore detailed descriptions, tantalizing food visuals, and customer reviews that will guide you towards making the perfect selection.
+      </h3>
+    </div>
+
+      
+         </body>
+</html>
